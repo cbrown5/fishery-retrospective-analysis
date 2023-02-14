397ef2a2c0489e91bfb4307e43f174819f8191e6
Chris Brown <christo.j.brown@gmail.com>
2023-02-14 20:01:16 +1000

    add trend bias figure

--- Scripts/10_two-year-trend-bias.R
+++ Scripts/10_two-year-trend-bias.R
@@ -12,14 +12,14 @@ load("Outputs/2022-02-11_processesed-assessment-data.rda")
 #axes settings 
 xmin <- 1980 #1980
 xmax <- 2020
-ymin <- 0
-ymax <- 1.5
+ymin <- -0.05
+ymax <- 0.05
 
-yaxis <- scale_y_continuous(breaks = seq(0, 1.5, by = 0.25),
+yaxis <- scale_y_continuous(breaks = seq(-0.05, 0.05, by = 0.01),
                             limits = c(ymin, ymax),
-                            labels = seq(0, 1.5, by = 0.25))
+                            labels = seq(-0.05, 0.05, by = 0.01))
 
-pal <- c("black", "#E69F00", "#56B4E9", "#009E73")#, "#0072B2")
+pal <- c("#E69F00", "#56B4E9", "#009E73")#, "#0072B2")
 
 #
 # Calculate trends and trend differences
@@ -83,16 +83,16 @@ dat_trend_diff2 <- dat_trend_diff %>%
 
 dat_assess_mean_status <- dat_trend_diff2 %>%
   group_by(years_to_MRY, assess_age, status) %>%
-  summarize(mean_trend = mean(trend_diff),
+  summarize(median_trend = median(trend_diff),
             n = n()) %>%
   filter(n>14) %>%
   ungroup() %>%
-  group_by(assess_age)
+  group_by(assess_age) %>%
+  filter(assess_age != "MRA")
 
 dat_assess_mean_status$assess_age <- 
   factor(dat_assess_mean_status$assess_age,
-         levels = c("MRA",
-                    "1-3 yr old",
+         levels = c("1-3 yr old",
                     "4-8 yr old",
                     ">8 yr old"))
 
@@ -102,31 +102,50 @@ g2 <-
   filter(status == "Sustainable") %>% 
   ggplot() + 
   #Filter out dip in final year (due to small sample bias)
-  aes(x = years_to_MRY, y = mean_trend,
-      color = assess_age, group = assess_age) +
+  aes(x = years_to_MRY, y = median_trend,
+      color = assess_age, group = assess_age,
+      fill = assess_age) +
+  geom_hline(yintercept = 0) +
   geom_line() +
+  stat_smooth(method = "gam", 
+              formula = y ~ s(x, bs = "tp")) + 
   theme_classic() + 
-  ylab("Trend mean difference") +
-  xlab("Year") + 
-  # yaxis +
+  ylab(expression(Delta*' trend')) +
+  xlab("Years before assessment") + 
+  xlim(0, 15) +
+  yaxis +
   scale_color_manual("Assessment age", 
                      values = 
-                       pal)
+                       pal) +
+  scale_fill_manual("Assessment age", 
+                     values = pal)
 
 g3 <- 
   dat_assess_mean_status %>%
   filter(status == "Depleted") %>% 
   ggplot() + 
   #Filter out dip in final year (due to small sample bias)
-  aes(x = years_to_MRY, y = mean_trend,
-      color = assess_age, group = assess_age) +
+  aes(x = years_to_MRY, y = median_trend,
+      color = assess_age, group = assess_age,
+      fill = assess_age) +
+  geom_hline(yintercept = 0) +
   geom_line() +
+  stat_smooth(method = "gam", 
+              formula = y ~ s(x, bs = "tp")) + 
   theme_classic() + 
-  ylab("Trend mean difference") +
-  xlab("Year") + 
-  # yaxis +
+  # ylab(expression(Delta*' trend')) +
+  xlab("Years before assessment") + 
+  xlim(0, 15) +
+  yaxis +
   scale_color_manual("Assessment age", 
                      values = 
-                       pal)
+                       pal) +
+  scale_fill_manual("Assessment age", 
+                    values = pal)
 
-g2 + g3
+gall <- g2 + g3 + 
+  plot_annotation(tag_levels = "A") + 
+  plot_layout(guides='collect') 
+gall
+ggsave("Outputs/bias-in-trends.png", gall,
+       width = 8, height = 4)
